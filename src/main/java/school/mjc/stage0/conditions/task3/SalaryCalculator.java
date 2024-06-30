91c932b0d9d0aae1e7e08a991f2679c9291ab6ca
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-06-30 17:32:29 +0500

    Resolved

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary > 0 && salary <= 10000) {
+            int taxes = salary * 15 / 100;
+            System.out.println("taxes - " + taxes);
+        } else if (salary > 10000 && salary <= 20000) {
+            int taxes = salary * 18 / 100;
+            System.out.println("taxes - " + taxes);
+        } else {
+            int taxes = salary * 20 / 100;
+            System.out.println("taxes - " + taxes);
+        }
     }
 }
